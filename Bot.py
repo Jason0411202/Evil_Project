db60e4f298e4aa935eeec425701ee44dd751a3dd
Daniel-lei <daniellei0303@gmail.com>
2023-09-14 20:49:03 +0800

    fix bugs about multiple pages

--- Bot.py
+++ Bot.py
@@ -10,14 +10,14 @@ from selenium import webdriver  # 需安裝selenium
 '''
 # 先到webdriver官網下載Win32的執行檔(符合自己Chrome的大版本)(chromedriver.exe)，再放到與此程式碼同一個資料夾中
 
-id = ''  # 必須修改成自己的帳號
-password = ''  # 必須修改成自己的密碼
+id = '410410101'  # 必須修改成自己的帳號
+password = '66977101'  # 必須修改成自己的密碼
 take_a_break = 0.5  # 當個有禮貌的網路爬蟲者，為了避免造成伺服器負擔，隨時休息一下
 course_Type = ['博雅通識']  # 可選:基礎通識,博雅通識 格式:[第一門課的資訊,第二門課的資訊]
-course_Dimention = ['自然科學與技術']# 可選:中國語文知識與應用,英文能力訓練,資訊能力課程,基礎概論課程, 格式:[第一門課的資訊,第二門課的資訊]
+course_Dimention = ['藝術與美學']# 可選:中國語文知識與應用,英文能力訓練,資訊能力課程,基礎概論課程, 格式:[第一門課的資訊,第二門課的資訊]
                                                          #      跨向度課程,藝術與美學,能源、環境與生態,人文思維與生命探索,公民與社會參與,經濟與國際脈動,自然科學與技術
 course_Page = [3]  # 要搶的課程在第幾頁 格式:[第一門課的資訊,第二門課的資訊]
-course_Position = [1]  # 要搶的課程在該頁的位置 格式:[第一門課的資訊,第二門課的資訊]
+course_Position = [4]  # 要搶的課程在該頁的位置 格式:[第一門課的資訊,第二門課的資訊]
 try_Times = 500000  # 嘗試搶課的次數
 time_To_Loggout = 3  # 循環幾次後要重登(注意「一次要搶的課數」*time_To_Loggout不可超過200)
 error_Counter = 0  # 出錯計數器
@@ -202,32 +202,34 @@ def Try_Get_Course(i):
 
 
 def main():
-    counter1 = 0
     counter2 = 0
-    while(counter1 < try_Times):
-        Login_User_Page()
-        counter2 = 0
-        while(counter2 < time_To_Loggout):
-            for i in range(len(course_Type)):
-                Send_Course_Seletion(i)
-                Try_Get_Course(i)
-                driver.switch_to.window(driver.window_handles[0])  # 跳至新的視窗
-            counter1 += 1
-            counter2 += 1
-            print('剩餘搶課的次數: ', try_Times-counter1)
-            print('距離重登的次數: ', time_To_Loggout-counter2)
-            print('')
-        driver.back()
+    Login_User_Page()
+    counter2 = 0
+    while(counter2 < time_To_Loggout):
+        for i in range(len(course_Type)):
+            Send_Course_Seletion(i)
+            Try_Get_Course(i)
+            driver.switch_to.window(driver.window_handles[0])  # 跳至新的視窗
+        counter2 += 1
+        print('剩餘搶課的次數: ', try_Times-counter1)
+        print('距離重登的次數: ', time_To_Loggout-counter2)
+        print('')
+    driver.quit()
+    return
+        
 
 # chromedriver.install()
 options = webdriver.ChromeOptions()  # 設定瀏覽器的選項
 options.add_argument("--start-maximized")  # 將放大視窗加入選項
 #options.add_argument('--headless') #不顯示瀏覽器UI
 
-driver = webdriver.Chrome(options=options)  # 開啟Win32的執行檔
+# driver = webdriver.Chrome(options=options)  # 開啟Win32的執行檔
 
-main()
-
-driver.quit()
+counter1=0
+while (counter1 < try_Times):
+    driver = webdriver.Chrome(options=options)  # 開啟Win32的執行檔
+    main()
+    counter1 += 1
+# driver.quit()
 
 # 版權警告:此程式為Graves Francis所有，不得抄襲，盜版必究
